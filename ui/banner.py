c3df43af0196f1d423bc81a495cc7a88f0ce8f5e
chamuzxmd-v2 <chamuzxmd@gmail.com>
2026-02-18 23:23:59 +0530

    Create banner.py

---
+++ ui/banner.py
@@ -0,0 +1,7 @@
+from colorama import Fore, Style
+import pyfiglet
+
+banner = pyfiglet.figlet_format("GCS TOOLBOX")
+print(Fore.RED + banner)
+print(Fore.GREEN + "Developers: ZORRO X ZADEX")
+print(Fore.YELLOW + "GHOST CYBER SQUAD OFC\n" + Style.RESET_ALL)
